80b156fada8f12beb5b8d0856eddee3a5801fb4b
Agostinho Carrara <97530020+agrandefamiliassh@users.noreply.github.com>
2022-06-30 19:40:02 -0300

    Add files via upload

--- SincAI.sh
+++ SincAI.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 clear
 echo -e "\033[1;36mHARMONIZANDO SERVIDOR E PAINEL WEB A.I ⚡\033[0m"
+sleep 10
 echo "America/Sao_Paulo" > /etc/timezone
 ln -fs /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 clear
 echo -e "\033[1;36mPOR FAVOR AGUARDE...\033[0m"
 echo -e "\033[1;36mDE UMA SEGURADA AI...\033[0m"
+sleep 3
 if [[ -e "/opt/sshplus/licenca.txt" ]]; then
 crack=$(cut -d"'" -f2 /opt/sshplus/licenca.txt) > /dev/null 2>&1
 fi
 sleep 1.5
 echo -e "\033[1;36mINJETANDO LICENÇA...\033[0m"
+sleep 2
 rm *.zip > /dev/null 2>&1
 apt install unzip -y > /dev/null 2>&1
 wget github.com/agrandefamiliassh/SincAI/raw/SincAI/sincarm/sshplus.zip.001 > /dev/null 2>&1
@@ -31,7 +34,7 @@ echo -e "\033[1;32mCaraca, nois e bom, se liga ai...\033[0m"
 sleep 3
 clear
 echo -e "\033[1;36mCONCLUIDO❤️\n1️⃣ VISITE SEU PAINEL WEB A.I ⚡.\n2️⃣ CRIE UM USUÁRIO.\nVOLTE NO PAINEL SSH E VERIFIQUE SE CRIOU.\033[0m"
-sleep 5
+sleep 10
 cat /dev/null > ~/.bash_history && history -c && clear
 rm *.html > /dev/null 2>&1
 rm *.zip > /dev/null 2>&1
