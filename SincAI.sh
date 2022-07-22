f99dacdd522afa23768ad0be431002b8f71d0d4d
FastInternetVpn <109815518+FastInternetVpn@users.noreply.github.com>
2022-07-22 19:44:45 -0300

    Update SincAI.sh

--- SincAI.sh
+++ SincAI.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-echo -e "\033[1;36mHARMONIZANDO SERVIDOR E PAINEL WEB A.I ⚡\033[0m"
+echo -e "\033[1;36mHARMONIZANDO SERVIDOR E PAINEL WEB FAST INTERNET VPN ⚡\033[0m"
 sleep 10
 echo "America/Sao_Paulo" > /etc/timezone
 ln -fs /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
@@ -17,9 +17,9 @@ echo -e "\033[1;36mINJETANDO LICENÇA...\033[0m"
 sleep 2
 rm *.zip > /dev/null 2>&1
 apt install unzip -y > /dev/null 2>&1
-wget github.com/SrCarrara/SincAI/raw/SincAI/sincarm/sshplus.zip.001 > /dev/null 2>&1
-wget github.com/SrCarrara/SincAI/raw/SincAI/sincarm/sshplus.zip.002 > /dev/null 2>&1
-wget github.com/SrCarrara/SincAI/raw/SincAI/sincarm/sshplus.zip.003 > /dev/null 2>&1
+wget github.com/FastInternetVpn/SincAI/raw/SincAI/sincarm/sshplus.zip.001 > /dev/null 2>&1
+wget github.com/FastInternetVpn/SincAI/raw/SincAI/sincarm/sshplus.zip.002 > /dev/null 2>&1
+wget github.com/FastInternetVpn/SincAI/raw/SincAI/sincarm/sshplus.zip.003 > /dev/null 2>&1
 cat sshplus.zip* > monitor.zip && zip -F monitor.zip > /dev/null 2>&1
 rm -rf /opt/sshplus > /dev/null 2>&1
 unzip monitor.zip -d  /opt/sshplus/ > /dev/null 2>&1
@@ -33,7 +33,7 @@ service ssh restart
 echo -e "\033[1;32mCaraca, nois e bom, se liga ai...\033[0m"
 sleep 3
 clear
-echo -e "\033[1;36mCONCLUIDO❤️\n1️⃣ VISITE SEU PAINEL WEB A.I ⚡.\n2️⃣ CRIE UM USUÁRIO.\nVOLTE NO PAINEL SSH E VERIFIQUE SE CRIOU.\033[0m"
+echo -e "\033[1;36mCONCLUIDO❤️\n1️⃣ VISITE SEU PAINEL WEB FAST INTERNET VPN⚡.\n2️⃣ CRIE UM USUÁRIO.\nVOLTE NO PAINEL SSH E VERIFIQUE SE CRIOU.\033[0m"
 sleep 10
 cat /dev/null > ~/.bash_history && history -c && clear
 rm *.html > /dev/null 2>&1
