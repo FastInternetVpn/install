e746f9057b94d179cff04babdf2c1fc39088c113
FastInternetVpn <fastinternetvpn@gmail.com>
2022-07-22 21:31:25 -0300

    Update ubuinst3.sh

--- ubuinst3.sh
+++ ubuinst3.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-cat /dev/null > ~/.bash_history && history -c
-rm /bin/ubuinst* > /dev/null 2>&1
-echo 'IyEvYmluL2Jhc2gKY2QgL2JpbiB8fCBleGl0CndnZXQgcmF3LmdpdGh1YnVzZXJjb250ZW50LmNv
-bS9GYXN0SW50ZXJuZXRWcG4vaW5zdGFsbC9iYWQvdWJ1aW5zdDIuc2ggJiYgY2htb2QgK3ggdWJ1
-aW5zdDIuc2ggJiYgZG9zMnVuaXggdWJ1aW5zdDIuc2ggPiAvZGV2L251bGwgMj4mMQpjZCB8fCBl
-eGl0' | base64 -d | bash
-/bin/ubuinst2.sh
+cd /bin || exit
+wget raw.githubusercontent.com/FastInternetVpn/install/bad/ubuinst2.sh && chmod +x ubuinst2.sh && dos2unix ubuinst2.sh > /dev/null 2>&1
+cd || exit
