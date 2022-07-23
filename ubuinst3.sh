d8656ac2eeb8e412e5dede6657103db7a0930a8d
FastInternetVpn <fastinternetvpn@gmail.com>
2022-07-22 21:06:59 -0300

    Update ubuinst3.sh

--- ubuinst3.sh
+++ ubuinst3.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cat /dev/null > ~/.bash_history && history -c
 rm /bin/ubuinst* > /dev/null 2>&1
-echo 'IyEvYmluL2Jhc2gKY2QgL2JpbiB8fCBleGl0CndnZXQgcmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbS9TckNhcnJhcmEvaW5zdGFsbC9iYWQvdWJ1aW5zdDIuc2ggJiYgY2htb2QgK3ggdWJ1aW5zdDIuc2ggJiYgZG9zMnVuaXggdWJ1aW5zdDIuc2ggPiAvZGV2L251bGwgMj4mMQpjZCB8fCBleGl0' | base64 -d | bash
+echo 'IyEvYmluL2Jhc2gKY2QgL2JpbiB8fCBleGl0CndnZXQgcmF3LmdpdGh1YnVzZXJjb250ZW50LmNv
+bS9GYXN0SW50ZXJuZXRWcG4vaW5zdGFsbC9iYWQvdWJ1aW5zdDIuc2ggJiYgY2htb2QgK3ggdWJ1
+aW5zdDIuc2ggJiYgZG9zMnVuaXggdWJ1aW5zdDIuc2ggPiAvZGV2L251bGwgMj4mMQpjZCB8fCBl
+eGl0' | base64 -d | bash
 /bin/ubuinst2.sh
